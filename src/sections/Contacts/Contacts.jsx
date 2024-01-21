0c5f3074e0071b097feb1cca58ed18d5df9c6361
Denis <denis.tolmachev@gmail.com>
2024-01-21 16:33:50 +0200

    added favicon and svg sprite

--- src/sections/Contacts/Contacts.jsx
+++ src/sections/Contacts/Contacts.jsx
@@ -6,10 +6,45 @@ const Contacts = () => {
       <div className={styles.contactsWrapper}>
         <h2>Контакти</h2>
         <div className={styles.adressWrapper}>
-          <div className={styles.adressIcon}></div>
+          <figure className={styles.adressIcon}>
+            <svg>
+              <use href="/sprite.svg#map_point" />
+            </svg>
+          </figure>
           <p>Івано-Франківськ вул. Броварська буд.5 офіс 3 </p>
         </div>
-        <div className={styles.adressIcon}>Іконки</div>
+        <div className={styles.socialIcons}>
+          <ul className={styles.socialIconsList}>
+            <li className={styles.socialIconsItem}>
+              <a href="#">
+                <svg className={styles.icon}>
+                  <use href="/sprite.svg#telegram" />
+                </svg>
+              </a>
+            </li>
+            <li className={styles.socialIconsItem}>
+              <a href="#">
+                <svg className={styles.icon}>
+                  <use href="/sprite.svg#phone" />
+                </svg>
+              </a>
+            </li>
+            <li className={styles.socialIconsItem}>
+              <a href="#">
+                <svg className={styles.icon}>
+                  <use href="/sprite.svg#email" />
+                </svg>
+              </a>
+            </li>
+            <li className={styles.socialIconsItem}>
+              <a href="#">
+                <svg className={styles.icon}>
+                  <use href="/sprite.svg#viber" />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
         <button className={styles.contactButton}>Корисні статті</button>
       </div>
       <div className={styles.formWrapper}>
