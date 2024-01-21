d8c4abde009758639739c0dc9481d4d5fbed3cca
Denis <denis.tolmachev@gmail.com>
2024-01-21 17:45:58 +0200

    added footer construcnion

---
+++ src/data/linksArr.js
@@ -0,0 +1,17 @@
+export const lincsArr = [
+  {
+    id: "1",
+    title: "Про мене",
+    href: "#about",
+  },
+  {
+    id: "2",
+    title: "Контакти",
+    href: "#contacts",
+  },
+  {
+    id: "3",
+    title: "Корисні статті",
+    href: "#",
+  },
+];
